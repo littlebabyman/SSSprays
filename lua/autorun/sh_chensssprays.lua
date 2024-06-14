c366ee6a19be95aab90c4381e834c16ffa71cba1
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2024-06-14 12:04:19 +0300

    more reliable worldspraying + use for stock spray

--- lua/autorun/sh_chensssprays.lua
+++ lua/autorun/sh_chensssprays.lua
@@ -4,21 +4,22 @@ if SERVER then
 	util.AddNetworkString("sssprays")
 	hook.Add("PlayerSpray", "SSSprays", function(ply)
 		if !game.SinglePlayer() and ply:GetPlayerInfo().customfiles[1] == "00000000" then return end
-		sound.Play("SprayCan.Paint", ply:EyePos() + ply:EyeAngles():Forward() * 16)
-		net.Start("sssprays")
-		net.WriteUInt(ply:UserID(),16)
-		net.Broadcast()
-		return true
+		if !ply:KeyDown(IN_USE) then return true end
 	end)
 	hook.Add("FinishMove", "SSSprays", function(ply, mv)
-		if mv:GetImpulseCommand() != 201 then return end
+		if mv:GetImpulseCommand() != 201 or mv:KeyDown(IN_USE) then return end
 		local trab = {}
 		trab.start = ply:EyePos()
 		trab.endpos = trab.start + ply:EyeAngles():Forward() * 128
 		trab.filter = ply
 		local tr = util.TraceLine(trab)
 		if !tr.Hit then return end
-		hook.Run("PlayerSpray", ply)
+		sound.Play("SprayCan.Paint", ply:EyePos() + ply:EyeAngles():Forward() * 16)
+		net.Start("sssprays")
+		net.WriteUInt(ply:UserID(),16)
+		net.WriteVector(ply:EyePos())
+		net.WriteAngle(ply:EyeAngles())
+		net.Broadcast()
 	end)
 end
 
@@ -29,7 +30,8 @@ if CLIENT then
 		local uid = net.ReadUInt(16)
 		ply = Player(uid)
 		if !IsValid(ply) then return end
-		local dir = ply:EyeAngles():Right()
+		local pos, ang = net.ReadVector(), net.ReadAngle()
+		local dir = ang:Right()
 		if !decalt[uid] then
 			local temp = ply:GetPlayerInfo().customfiles[1]
 			local cfile = "user_custom/" .. string.Left(temp, 2) .. "/" .. temp .. ".dat"
@@ -63,7 +65,7 @@ if CLIENT then
 			spray:SetFloat("$decalscale", 64 / spray:Width())
 			decalt[uid] = spray
 		end
-		local qt = util.QuickTrace(ply:EyePos(), ply:GetAimVector() * 256, ply)
+		local qt = util.QuickTrace(pos, ang:Forward() * 128, ply)
 		if !qt.Hit then return end
 		if qt.HitTexture ==  "**studio**" then dir = qt.HitNormal-qt.Normal end
 		util.DecalEx(decalt[uid], qt.Entity, qt.HitPos+qt.HitNormal, dir, color_white, 1, 1)
